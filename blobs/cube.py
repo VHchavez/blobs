5623f6562ecd0ed74c49c984af3ae48277faaea0
Victor H. Chavez <gonza445@purdue.edu>
2019-12-05 12:34:34 -0500

    yapf cube.py

--- blobs/cube.py
+++ blobs/cube.py
@@ -14,17 +14,17 @@ import plotly.express as px
 class Manager():
     def __init__(self, wfn):
         psi4.cubeprop(wfn)
-        self.wfn      = wfn   
+        self.wfn = wfn
         self.geometry = self.wfn.molecule()
-        self.spacing  = psi4.core.get_global_option("CUBIC_GRID_SPACING") 
-        self.overage  = psi4.core.get_global_option("CUBIC_GRID_OVERAGE") 
-        self.origin   = self.get_origin()
-        self.info     = self.get_info() 
-        self.meta     = None
+        self.spacing = psi4.core.get_global_option("CUBIC_GRID_SPACING")
+        self.overage = psi4.core.get_global_option("CUBIC_GRID_OVERAGE")
+        self.origin = self.get_origin()
+        self.info = self.get_info()
+        self.meta = None
 
     def get_origin(self):
         geometry = self.geometry.full_geometry().np
-        
+
         L = self.overage
         D = self.spacing
 
@@ -35,8 +35,8 @@ class Manager():
         N = np.zeros(3)
         O = np.zeros(3)
 
-        for k in [0,1,2]:
-            Xmin[k] = Xmax[k] = geometry[0,k]
+        for k in [0, 1, 2]:
+            Xmin[k] = Xmax[k] = geometry[0, k]
 
             for atom in range(len(geometry)):
                 Xmin[k] = geometry[atom, k] if Xmin[k] > geometry[atom, k] else Xmin[k]
@@ -52,41 +52,46 @@ class Manager():
 
         return O
 
-
     def get_info(self):
 
         symbol = []
-        x_geo  = []
-        y_geo  = []
-        z_geo  = []
-        color  = []
-        size   = []
+        x_geo = []
+        y_geo = []
+        z_geo = []
+        color = []
+        size = []
         atoms_colors = get_colors()
 
         for i in range(self.geometry.natom()):
             symbol.append(self.geometry.fsymbol(i))
-            x_geo.append(int((self.geometry.fx(i) - self.origin[0])/self.spacing[0]))
-            y_geo.append(int((self.geometry.fy(i) - self.origin[1])/self.spacing[1]))
-            z_geo.append(int((self.geometry.fz(i) - self.origin[2])/self.spacing[2]))
-        
+            x_geo.append(int((self.geometry.fx(i) - self.origin[0]) / self.spacing[0]))
+            y_geo.append(int((self.geometry.fy(i) - self.origin[1]) / self.spacing[1]))
+            z_geo.append(int((self.geometry.fz(i) - self.origin[2]) / self.spacing[2]))
+
             for atom in atoms_colors:
                 if self.geometry.fsymbol(i) == atom:
                     color.append(atoms_colors[atom][0])
                     size.append(atoms_colors[atom][1] + 15)
 
         mol_dic = {
-            "sym"   : symbol, 
-            "x"     : np.array(x_geo), 
-            "y"     : np.array(y_geo), 
-            "z"     : np.array(z_geo), 
-            "color" : color,
-            "size"  : np.array(size)
+            "sym": symbol,
+            "x": np.array(x_geo),
+            "y": np.array(y_geo),
+            "z": np.array(z_geo),
+            "color": color,
+            "size": np.array(size)
         }
-        
+
         return mol_dic
 
-
-    def plot(self, cube_file, iso=0.03, cube_type="density", colorscale="Blues", size=1, plot_geometry=True, plot_bonds=True):
+    def plot(self,
+             cube_file,
+             iso=0.03,
+             cube_type="density",
+             colorscale="Blues",
+             size=1,
+             plot_geometry=True,
+             plot_bonds=True):
 
         cube, meta = cube_to_array(cube_file)
         self.meta = meta
@@ -95,118 +100,119 @@ class Manager():
 
         data = []
 
-        vol_data = go.Isosurface(
-            x=X.flatten(), y=Y.flatten(), z=Z.flatten(),
-            value=cube.flatten(),
-            showscale=False,
-            surface_count=2,
-            isomax=iso,
-            isomin=iso,
-            opacity=0.2,
-            colorscale=colorscale)
+        vol_data = go.Isosurface(x=X.flatten(),
+                                 y=Y.flatten(),
+                                 z=Z.flatten(),
+                                 value=cube.flatten(),
+                                 showscale=False,
+                                 surface_count=2,
+                                 isomax=iso,
+                                 isomin=iso,
+                                 opacity=0.2,
+                                 colorscale=colorscale)
 
         data.append(vol_data)
 
-        if cube_type=="orbital":
-            vol_data_neg = go.Isosurface(
-            x=X.flatten(), y=Y.flatten(), z=Z.flatten(),
-            value=cube.flatten(),
-            showscale=False, 
-            isomin=-1*iso,
-            isomax=-1*iso,
-            opacity=0.2, 
-            colorscale="Reds")
-        
+        if cube_type == "orbital":
+            vol_data_neg = go.Isosurface(x=X.flatten(),
+                                         y=Y.flatten(),
+                                         z=Z.flatten(),
+                                         value=cube.flatten(),
+                                         showscale=False,
+                                         isomin=-1 * iso,
+                                         isomax=-1 * iso,
+                                         opacity=0.2,
+                                         colorscale="Reds")
+
             data.append(vol_data_neg)
 
+        if plot_geometry == True:
 
-        if plot_geometry==True:
+            geo_data = go.Scatter3d(x=self.info["x"],
+                                    y=self.info["y"],
+                                    z=self.info["z"],
+                                    mode="markers",
+                                    marker={
+                                        "showscale": False,
+                                        "color": self.info["color"],
+                                        "size": self.info["size"] * size / 1.0,
+                                        "showscale": False,
+                                        "opacity": 1.0,
+                                        "line": {
+                                            "width": 2,
+                                            "color": "gainsboro"
+                                        }
+                                    })
 
-            geo_data = go.Scatter3d(
-            x=self.info["x"], y=self.info["y"], z=self.info["z"],
-            mode="markers",
-            marker={"showscale":False, 
-                    "color":self.info["color"], 
-                    "size":self.info["size"] * size/1.0,
-                    "showscale": False,
-                    "opacity":1.0, 
-                    "line": {"width":2, "color":"gainsboro"}})
-            
             data.append(geo_data)
 
-        fig = go.Figure(data=data)  
+        fig = go.Figure(data=data)
 
-        if plot_bonds==True:
+        if plot_bonds == True:
 
             bonds = build_bond_list(self.geometry.geometry().np)
 
             for i in range(len(bonds)):
-    
-                bond_x = [self.info["x"][bonds[i][0]] , self.info["x"][bonds[i][1]] ]
-                bond_y = [self.info["y"][bonds[i][0]] , self.info["y"][bonds[i][1]] ]
-                bond_z = [self.info["z"][bonds[i][0]] , self.info["z"][bonds[i][1]] ]    
-                
-                fig.add_trace(go.Scatter3d(x = bond_x, y = bond_y, z = bond_z, 
-                            mode = "lines", 
-                            line={"color":"grey", 
-                                  "width":2},
-                            ))  
 
+                bond_x = [self.info["x"][bonds[i][0]], self.info["x"][bonds[i][1]]]
+                bond_y = [self.info["y"][bonds[i][0]], self.info["y"][bonds[i][1]]]
+                bond_z = [self.info["z"][bonds[i][0]], self.info["z"][bonds[i][1]]]
 
-        layout = go.layout.Template(
-            layout=go.Layout(title_font=dict(family="Rockwell", size=24)))
+                fig.add_trace(
+                    go.Scatter3d(
+                        x=bond_x,
+                        y=bond_y,
+                        z=bond_z,
+                        mode="lines",
+                        line={
+                            "color": "grey",
+                            "width": 2
+                        },
+                    ))
 
+        layout = go.layout.Template(layout=go.Layout(title_font=dict(family="Rockwell", size=24)))
 
         fig.update_layout(scene_xaxis_showticklabels=False,
-                    scene_yaxis_showticklabels=False,
-                    scene_zaxis_showticklabels=False, 
-                    dragmode="orbit", 
-                    width=size * 500, 
-                    height=size * 500, 
-                    template="plotly_white",
-                    autosize = True, 
-                    showlegend=False, 
-                    hovermode=False,
-                    xaxis=dict(
-                                autorange=True,
-                                showgrid=False,
-                                ticks='',
-                                showticklabels=False
-                            ),
-                    yaxis=dict(
-                                autorange=True,
-                                showgrid=False,
-                                ticks='',
-                                showticklabels=False
-                    ),
-                     scene = {
-                                "xaxis": {
-                                "autorange": True,
-                                "showgrid": False,
-                                "zeroline": False,
-                                "showline": False,
-                                "title": "",
-                                "ticks": '',
-                                "showticklabels": False
-                            },
-                                "yaxis": {
-                                    "autorange": True,
-                                    "showgrid": False,
-                                    "zeroline": False,
-                                    "showline": False,
-                                    "title": "",
-                                    "ticks": '',
-                                    "showticklabels": False
-                                },
-                                "zaxis": {
-                                    "autorange": True,
-                                    "showgrid": False,
-                                    "zeroline": False,
-                                    "showline": False,
-                                    "title": "",
-                                    "ticks": '',
-                                    "showticklabels": False
-                                }})
+                          scene_yaxis_showticklabels=False,
+                          scene_zaxis_showticklabels=False,
+                          dragmode="orbit",
+                          width=size * 500,
+                          height=size * 500,
+                          template="plotly_white",
+                          autosize=True,
+                          showlegend=False,
+                          hovermode=False,
+                          xaxis=dict(autorange=True, showgrid=False, ticks='', showticklabels=False),
+                          yaxis=dict(autorange=True, showgrid=False, ticks='', showticklabels=False),
+                          scene={
+                              "xaxis": {
+                                  "autorange": True,
+                                  "showgrid": False,
+                                  "zeroline": False,
+                                  "showline": False,
+                                  "title": "",
+                                  "ticks": '',
+                                  "showticklabels": False
+                              },
+                              "yaxis": {
+                                  "autorange": True,
+                                  "showgrid": False,
+                                  "zeroline": False,
+                                  "showline": False,
+                                  "title": "",
+                                  "ticks": '',
+                                  "showticklabels": False
+                              },
+                              "zaxis": {
+                                  "autorange": True,
+                                  "showgrid": False,
+                                  "zeroline": False,
+                                  "showline": False,
+                                  "title": "",
+                                  "ticks": '',
+                                  "showticklabels": False
+                              }
+                          })
 
         fig.show(config={'scrollZoom': False})
 
@@ -230,6 +236,7 @@ def _getline(cube):
     l = cube.readline().strip().split()
     return int(l[0]), list(map(float, l[1:]))
 
+
 def cube_to_array(fname):
     """
     Read cube file into numpy array
@@ -245,13 +252,14 @@ def cube_to_array(fname):
     """
     cube_details = {}
     with open(fname, 'r') as cube:
-        cube.readline(); cube.readline()  # ignore comments
+        cube.readline()
+        cube.readline()  # ignore comments
         natm, cube_details['org'] = _getline(cube)
         nx, cube_details['xvec'] = _getline(cube)
         ny, cube_details['yvec'] = _getline(cube)
         nz, cube_details['zvec'] = _getline(cube)
         cube_details['atoms'] = [_getline(cube) for i in range(natm)]
-        data = np.zeros((nx*ny*nz))
+        data = np.zeros((nx * ny * nz))
         idx = 0
         for line in cube:
             for val in line.strip().split():
@@ -264,9 +272,10 @@ def cube_to_array(fname):
 
 def calculate_distance(rA, rB):
     """Calculate the distance between points A and B. Assumes rA and rB are numpy arrays."""
-    dist_vec = (rA-rB)
+    dist_vec = (rA - rB)
     distance = np.linalg.norm(dist_vec)
     return distance
+
 
 def calculate_distance_list(rA, rB):
     """Calculate the distance between points A and B. Assums rA and rB are lists."""
@@ -276,6 +285,7 @@ def calculate_distance_list(rA, rB):
 
     distance = np.sqrt(squared_sum)
     return distance
+
 
 def build_bond_list(coordinates, max_bond=2.7, min_bond=0):
     """ Build list of bonds from atomic coordinates based on distance.
